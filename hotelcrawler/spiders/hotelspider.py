020c5acddd643f2971ac1d2c9172270768d2b1b6
simeon.jolo@gmail.com <simeon.jolo@gmail.com>
2018-06-21 02:13:35 +0800

    ang bagal wtf

--- hotelcrawler/spiders/hotelspider.py
+++ hotelcrawler/spiders/hotelspider.py
@@ -21,11 +21,13 @@ class HotelSpider(scrapy.Spider):
     #start_urls = ['https://www.booking.com/hotel/ph/red-planet-manila-bay-manila.en-gb.html?aid=304142;label=gen173nr-1FCAEoggJCAlhYSDNYBGi0AYgBAZgBLsIBCndpbmRvd3MgMTDIAQ_YAQHoAQH4AQuSAgF5qAID;sid=d1cefe2b1ccb1dd74325d599983f140c;all_sr_blocks=266128901_105636671_2_0_0;checkin=2018-09-13;checkout=2018-09-14;dest_id=-2437894;dest_type=city;dist=0;group_adults=1;group_children=0;hapos=1;highlighted_blocks=266128901_105636671_2_0_0;hpos=1;no_rooms=1;room1=A;sb_price_type=total;srepoch=1528865568;srfid=240de3bab9026fe7fa53e68e5358b24fe5f4d157X1;srpvid=63e5224fd68c002c;type=total;ucfs=1&#hotelTmpl']
     #start_urls = ['https://www.booking.com/searchresults.en-gb.html?label=gen173nr-1FCAQoggJCC3JlZ2lvbl81MjU4SAlYBGi0AYgBAZgBLsIBCndpbmRvd3MgMTDIAQ_YAQHoAQH4AQKSAgF5qAID&sid=d42d69c201c210da5ba401e54684cb03&sb=1&src=searchresults&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Fsearchresults.en-gb.html%3Flabel%3Dgen173nr-1FCAQoggJCC3JlZ2lvbl81MjU4SAlYBGi0AYgBAZgBLsIBCndpbmRvd3MgMTDIAQ_YAQHoAQH4AQKSAgF5qAID%3Bsid%3Dd42d69c201c210da5ba401e54684cb03%3Bcity%3D-2437894%3Bclass_interval%3D1%3Bdest_id%3D5258%3Bdest_type%3Dregion%3Bdtdisc%3D0%3Binac%3D0%3Bindex_postcard%3D0%3Blabel_click%3Dundef%3Bnflt%3Dclass%253D5%253Bclass%253D4%253Bclass%253D3%253Bclass%253D2%253Bclass%253D1%253B%253Bht_id%253D204%3Boffset%3D0%3Bpostcard%3D0%3Braw_dest_type%3Dregion%3Broom1%3DA%252CA%3Bsb_price_type%3Dtotal%3Bss%3DMetro%2520Manila%252C%2520Philippines%3Bss_all%3D0%3Bssb%3Dempty%3Bsshis%3D0%26%3B&ss=Metro+Manila&ssne=Metro+Manila&ssne_untouched=Metro+Manila&region=5258&checkin_monthday=13&checkin_month=6&checkin_year=2018&checkout_monthday=14&checkout_month=6&checkout_year=2018&group_adults=1&group_children=0&no_rooms=1&from_sf=1']
     start_urls = ['https://www.booking.com/searchresults.en-gb.html?region=5258;ss=Metro%2BManila']
+    #start_urls = ['https://www.booking.com/searchresults.en-gb.html?region=5630;ss=Davao%2C%20Philippines']
     #start_urls = ['https://www.booking.com/hotel/ph/red-planet-binondo.en-gb.html?aid=304142;label=gen173nr-1FCAQoggJCE3NlYXJjaF9tZXRybyttYW5pbGFICVgEaLQBiAEBmAEuwgEKd2luZG93cyAxMMgBDNgBAegBAfgBApICAXmoAgM;sid=78e774a890731222a7398aead8e3d850;all_sr_blocks=266128701_105636643_2_0_0;checkin=2018-06-21;checkout=2018-06-22;dest_id=-2437894;dest_type=city;dist=0;dotd_fb=1;hapos=1;highlighted_blocks=266128701_105636643_2_0_0;hpos=1;nflt=class%3D1%3Bclass%3D2%3Bclass%3D3%3Bclass%3D4%3Bclass%3D5%3Bht_id%3D204;room1=A;sb_price_type=total;srepoch=1528913348;srfid=69eec472f4147f110a73f67557b310b05c8bc005X1;srpvid=f87d7fa1a7f00149;type=total;ucfs=1&#hotelTmpl']
 
     def __init__(self):
         chrome_options = Options()
-        #chrome_options.add_argument("--headless")
+       # chrome_options.add_argument("--headless")
+       # chrome_options.add_argument("--window-size=1400x900")
         prefs = {'profile.managed_default_content_settings.images':2}
         chrome_options.add_experimental_option("detach", True)
         chrome_options.add_experimental_option("prefs", prefs)
@@ -46,11 +48,17 @@ class HotelSpider(scrapy.Spider):
         wait.until(EC.presence_of_element_located((By.XPATH, '//span[contains(text(), "1 star")]')))
 
         self.driver.find_element_by_xpath('//span[contains(text(), "1 star")]').click()
+        time.sleep(1)
         self.driver.find_element_by_xpath('//span[contains(text(), "2 stars")]').click()
+        time.sleep(1)
         self.driver.find_element_by_xpath('//span[contains(text(), "3 stars")]').click()
+        time.sleep(1)
         self.driver.find_element_by_xpath('//span[contains(text(), "4 stars")]').click()
+        time.sleep(1)
         self.driver.find_element_by_xpath('//span[contains(text(), "5 stars")]').click()
+        time.sleep(1)
         self.driver.find_element_by_xpath('//div[contains(@class, "filter_item")]/span[contains(text(), "Hotels")]').click()
+        time.sleep(1)
         #self.driver.find_element_by_xpath('//li[contains(@class, "sort_distance_from_landmark")]/a').click()
         self.driver.find_element_by_xpath('//li[contains(@class, "sort_bayesian_review_score")]/a').click()
 
@@ -63,7 +71,7 @@ class HotelSpider(scrapy.Spider):
         resultsParsed = 0
         
         reqResults = -1
-        reqResults = 5
+        #reqResults = 2
         stopParse = False
 
         while reqResults == -1 or resultsParsed < reqResults:
@@ -93,9 +101,10 @@ class HotelSpider(scrapy.Spider):
         print (len(hotels_list))
         hotelsParsed = 0
         for url, location_from_center in hotels_list.items():
-            offset_list = [120]
-            #offset_list = [1, 2, 4, 5, 7, 14, 20, 25, 30, 35, 40, 45, 50, 55, 60, 70, 80, 90, 120]
-
+            print("*********************Hotel " + str(hotelsParsed + 1) + " out of " + str(len(hotels_list)))
+            
+            offset_list = [1, 2, 4, 5, 7, 14, 20, 25, 30, 35, 40, 45, 50, 55, 60, 70, 80, 90, 120]
+            #offset_list = [1, 120]
             for days_offset in offset_list:
                 checkin_date = (datetime.datetime.today() + timedelta(days = days_offset)).strftime('%Y-%m-%d')
                 checkout_date = (datetime.datetime.today() + timedelta(days = days_offset + 1)).strftime('%Y-%m-%d')
@@ -138,16 +147,19 @@ class HotelSpider(scrapy.Spider):
                         has_breakfast_buffet = 1
                     
                     #look for rooms
-                    rooms_list = response.xpath('//table[contains(@class, "hprt-table")]//tr')
-                    #for every room
-                    for room in rooms_list:
-                        room_type =  unicodedata.normalize("NFKD", room.xpath('normalize-space(.//span[contains(@class, "hprt-roomtype-icon-link")]/text())').extract_first())
-                        price =  unicodedata.normalize("NFKD", room.xpath('normalize-space(.//span[contains(@class, "hprt-price-price-standard")]/text())').extract_first())
+                rooms_list = response.xpath('//table[contains(@class, "hprt-table")]//tr')
+                #for every room
+                for room in rooms_list:
+                    room_type =  unicodedata.normalize("NFKD", room.xpath('normalize-space(.//span[contains(@class, "hprt-roomtype-icon-link")]/text())').extract_first())
+                    price =  unicodedata.normalize("NFKD", room.xpath('normalize-space(.//span[contains(@class, "hprt-price-price-standard")]/text())').extract_first())
+                    if not price:
+                        price =  unicodedata.normalize("NFKD", room.xpath('normalize-space(.//span[contains(@class, "hprt-price-price-actual")]/text())').extract_first())
                         if not price:
-                            price =  unicodedata.normalize("NFKD", room.xpath('normalize-space(.//span[contains(@class, "hprt-price-price-actual")]/text())').extract_first())
-                            if not price:
-                                price =  unicodedata.normalize("NFKD", room.xpath('normalize-space(.//span[contains(@class, "bui-price__title-small")]/text())').extract_first())
-                        if room_type and price:
+                            price =  unicodedata.normalize("NFKD", room.xpath('normalize-space(.//span[contains(@class, "bui-price__title-small")]/text())').extract_first())
+                    if room_type and price:
+                        capacityNum = len(room.xpath('.//div[contains(@class, "hprt-occupancy-occupancy-info")]//i'))
+                        capacity = str(len(room.xpath('.//div[contains(@class, "hprt-occupancy-occupancy-info")]//i'))),
+                        if (capacityNum <= 2):
                             ##yield for every room entry in available room list
                             yield {
                                 'hotel_name': hotel_name,
@@ -158,6 +170,7 @@ class HotelSpider(scrapy.Spider):
                                 'check-out-date': checkout_date,
                                 'room_type': room_type,
                                 'price': price,
+                                'capacity': capacity,
                                 'location_from_center': location_from_center,
                                 'is_hotel_chain': is_hotel_chain,
                                 'has_breakfast_buffet': has_breakfast_buffet,
